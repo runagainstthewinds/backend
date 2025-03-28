1ca81d5f07246422b6f47f03851cc72cb881a270
David Zhou <david.zhou.0110@gmail.com>
2025-03-28 01:08:59 -0400

    FIX: Fixed user to require username, email, password

--- src/test/java/com/example/RunAgainstTheWind/applicationTesting/JwtFilterTest.java
+++ src/test/java/com/example/RunAgainstTheWind/applicationTesting/JwtFilterTest.java
@@ -1,4 +1,4 @@
-package com.example.RunAgainstTheWind.application;
+package com.example.RunAgainstTheWind.applicationTesting;
 
 import com.example.RunAgainstTheWind.application.user.model.Users;
 import com.fasterxml.jackson.databind.ObjectMapper;
