5252518d622ce17210b2df9f67553081cd430603
David Zhou <david.zhou.0110@gmail.com>
2025-04-10 01:07:36 -0400

    Save

--- src/main/java/com/example/RunAgainstTheWind/domain/userDetails/service/UserDetailsService.java
+++ src/main/java/com/example/RunAgainstTheWind/domain/userDetails/service/UserDetailsService.java
@@ -1,5 +1,32 @@
 package com.example.RunAgainstTheWind.domain.userDetails.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.RunAgainstTheWind.domain.user.repository.UserRepository;
+import com.example.RunAgainstTheWind.domain.userDetails.model.UserDetails;
+import com.example.RunAgainstTheWind.domain.userDetails.repository.UserDetailsRepository;
+import com.example.RunAgainstTheWind.dto.UserDetailsDTO;
+
+@Service
 public class UserDetailsService {
     
+    private final UserDetailsRepository userDetailsRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsService(UserDetailsRepository userDetailsRepository, UserRepository userRepository) {
+        this.userDetailsRepository = userDetailsRepository;
+        this.userRepository = userRepository;
+    }
+
+    public UserDetailsDTO getUserDetailsById(Long userId) {
+        Long userDetailsId = userRepository.findById(userId.intValue()).map(user -> user.getUserDetails().getUserDetailsId()).orElse(null);
+        UserDetails userDetails = userDetailsRepository.findById(userId).orElse(null);
+
+        UserDetailsDTO dto = new UserDetailsDTO();
+        dto.setUserDetailsId(userDetailsId);
+    }
+
+
 }
---
+++ src/main/java/com/example/RunAgainstTheWind/dto/UserDetailsDTO.java
@@ -0,0 +1,12 @@
+package com.example.RunAgainstTheWind.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDetailsDTO {
+    private Long userDetailsId;
+    private Double totalDistance;
+    private Double totalDuration;
+    private Double weeklyDistance;
+    private Integer runCount;
+}
